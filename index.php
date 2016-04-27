5c943e57fbad1e1fceff7d9e7d421c1ee2ffc9ec
Josh Young <j22cal@gmail.com>
2016-04-27 18:06:54 -0500

    Adding comment form back. Disabling comment submit button.

--- index.php
+++ index.php
@@ -31,11 +31,11 @@
 <div id="containerTop">
 	<div id="subContTop">
 		<div class="content">
-			<a name="home"></a> 
+			<a name="home"></a>
 			<div id="header">
 				<h1 id="logo"><a href='http://www.codeitcreations.com/'>Code-it Creations</a></h1>
 				<h2 id="slogan">Transforming Code into Beautiful Sites</h2>
-				
+
 				<ul id="nav">
 					<li><a href="#work">Work</a> &middot; </li>
 					<li><a href="#port">Portfolio</a> &middot; </li>
@@ -43,44 +43,44 @@
 					<li><a href="#contact">Contact</a></li>
 				</ul>
 			</div><!-- E_header -->
-			
+
 			<p class="theError errorWrap"></p>
-			
+
 				<div class="section top">
 					<div class="rounded">
 						<div class="rLeft"></div>
 						<div class="middle"><a name="process"></a></div>
 						<div class="rRight"></div>
 					</div><!-- E_rounded -->
-					
+
 					<div class="sub struct">
 						<h1 class="wedo">Our Goal</h1>
-						<p>At Code-it Creations, focusing on the important details of your project is critical to us. Our goal 
-						is to present each client with an ideal web presence that reflects his true needs. Ultimately, 
+						<p>At Code-it Creations, focusing on the important details of your project is critical to us. Our goal
+						is to present each client with an ideal web presence that reflects his true needs. Ultimately,
 						we strive to deliver appealing, user-friendly sites at affordable prices.</p>
 					</div>
 					<div class="sub process">
 						<h1 class="struct">Our Process<span>explained</span></h1>
-						<p>Although appealing sites are important to us, each of our web solutions is 
-						built to be extremely maintainable. With this in mind, we implement each site into a content 
+						<p>Although appealing sites are important to us, each of our web solutions is
+						built to be extremely maintainable. With this in mind, we implement each site into a content
 						management system, allowing our clients to modify their site content as needed.</p>
 					</div>
 					<div class="sub last wedo">
 						<h1 class="process">Our Work<span>is</span></h1>
-						<p>Our sites are built using the latest web standards and optimized for their browsing 
-						environments. Whether code or images, we utilize the tools necessary to create sites that are not 
+						<p>Our sites are built using the latest web standards and optimized for their browsing
+						environments. Whether code or images, we utilize the tools necessary to create sites that are not
 						only engaging, but structurally sound, as well. Take a look at our work below.</p>
 					</div>
 				</div><!-- E_section -->
-				
+
 				<div class="section next">
-					
+
                 <div class="rounded">
                     <div class="rLeft"></div>
                     <div class="middle"><a name="work"></a></div>
 					<div class="rRight"></div>
                 </div><!-- E_rounded -->
-					
+
 					<h1>Recent Projects</h1>
 					<div class="imgCont">
 						<a href="images/comments_nl.jpg" title="The Whistle Stop Inn"><img src="images/comments_n.jpg" alt="Comments &amp; Capers" /></a>
@@ -96,7 +96,7 @@
 						</div><!-- E_sub -->
 						<div class="sub">
 							<h2>My Portfolio (old design)</h2>
-							<p>Features XHTML, CSS, jQuery, and PHP. This 
+							<p>Features XHTML, CSS, jQuery, and PHP. This
 							is an older implementatin of my personal portfolio.
 							</p>
 							   <a href="http://www.codeitcreations.com" rel='external'>Visit Site</a>
@@ -108,20 +108,20 @@
 							   <a href="http://www.codeitcreations.com" rel='external'>Visit Site</a>
 						</div><!-- E_sub -->
 					</div><!-- E_imgDescCont -->
-					
+
 					<div id="portLinkCont">
 						<a name="port"></a>
 						<img id="expand" src="images/expand.png" alt="" />
 					</div><!-- E_portLinkCont -->
-							
+
 				</div><!-- E_section -->
-		
-		</div><!-- E_content -->	
-	<div><!-- E_subContTop -->	
+
+		</div><!-- E_content -->
+	<div><!-- E_subContTop -->
 </div><!-- E_containerTop -->
 
 	<div id="morePortfolio">
-		<div class="content"> 
+		<div class="content">
 			<div class="section next">
 				<div class="imgCont">
 						<a href="images/dogGonel.jpg" title="Dog Gone Walking"><img src="images/dogGone.jpg" alt="Dog Gone Walking" /></a>
@@ -132,7 +132,7 @@
 						<div class="sub">
 							<h2>Dog Gone Walking</h2>
 							<p>Build on top of WordPress, via XHTML and CSS templating.
-							   Not designed by Code-it Creations, but taken from PSD to 
+							   Not designed by Code-it Creations, but taken from PSD to
 							   a fully usable form.
 							</p>
 						</div><!-- E_sub -->
@@ -144,7 +144,7 @@
 						</div><!-- E_sub -->
 						<div class="sub last">
 							<h2>Comments &amp; Capers (older layout)</h2>
-							<p>Features XHTML, CSS, jQuery, and PHP. Contains 
+							<p>Features XHTML, CSS, jQuery, and PHP. Contains
 							custom PHP code to allow data to be added to the site.
 							</p>
 							   <a href="http://www.commentsandcapers.com" rel='external'>Visit Site</a>
@@ -157,21 +157,21 @@
 					<div class="imgDescCont">
 						<div class="sub">
 							<h2>Code-it Creations (old design)</h2>
-							<p>Precursor to the example listed above with a few color 
-							and organizational changes. 
+							<p>Precursor to the example listed above with a few color
+							and organizational changes.
 							</p>
 						</div><!-- E_sub -->
 						<div class="sub last">
 							<h2>Comments &amp; Capers</h2>
-							<p>An older version of Comments &amp; Capers. Features a liquid based layout. 
+							<p>An older version of Comments &amp; Capers. Features a liquid based layout.
 							Later restructured to allow shorter lines of text which facilitate easier reading.</p>
 						</div><!-- E_sub -->
 					</div><!-- E_imgDescCont -->
-					
-					<p class='disclaimer'>Each of our client's has control over his own site; therefore, if a site looks different than pictured here, 
+
+					<p class='disclaimer'>Each of our client's has control over his own site; therefore, if a site looks different than pictured here,
 					this is due to changes made by the client.</p>
 				</div><!-- E_section -->
-				
+
 		</div><!-- E_content -->
 	</div><!-- E_morePortfolio -->
 
@@ -181,11 +181,11 @@
 				<div class="section contact">
 				<div id="plus"></div>
                 <div class="rounded">
-                    <div class="rLeft"></div><a name="contact"></a> 
+                    <div class="rLeft"></div><a name="contact"></a>
                     <div class="middle"><a name="contact"></a></div>
                     <div class="rRight"></div>
                 </div><!-- E_rounded -->
-                
+
 					<h1>Contact Details</h1>
 					<div id="address">
 						<p>
@@ -197,12 +197,41 @@
 					<div id="contactBox">
 						<h1>Got a Question / Need an Estimate?</h1>
 						<?php include('scripts/functions.php'); ?>
+						<form action="" method="post">
+							<fieldset>
+								<div class="field required">
+									<label for="name">Your Name</label>
+									<input type="text" name="name" id="name" class="text verifyText" title="*first last" />
+									<span class="iferror">Field required!</span>
+								</div><!-- E_field -->
+
+								<div class="field required\">
+								<label for="email">Your Email</label>
+								<input type="text" id="email" name="email" class="text verifyMail" title="*example@email.com" />
+								<span class="iferror">Not a valid email address!</span>
+								</div><!-- E_field -->
+
+								<div class="field required">
+								<label for="site">Your Site</label>
+								<input type="text" id="site" name="site" class="text verifyURL" title="*www.example.com" />
+								<span class="iferror">Not a valid web address!</span>
+								</div><!-- E_field -->
+
+								<input type='text' name='contact_empty' value='' class='hide' />
+
+								<label for="description">Work Needed</label>
+								<textarea name="description" id="description" cols="10" rows="10">
+								</textarea>
+
+								<input class="button" type="submit" value="Submit" name="submit" />
+							</fieldset>
+						</form>
 					</div><!-- E_contactBox -->
 				</div><!-- E_section -->
-				
+
 				<div class="section bottom">
 					<div id="footerCont">
-						<a name="about"></a> 
+						<a name="about"></a>
 						<div class="sub">
 							<h4>About the Developer</h4>
 							<p>As the founder and creator of Code-it Creations, Josh Young
@@ -217,16 +246,16 @@
 							<h4>Philosophy</h4>
 							<p>At Code-it Creations, our mission is to develop sites
 							   that are affordable to the average consumer. All of our web solutions
-							   are hand-coded and developed for the unique needs of the 
-							   user, using the best technologies and programming languages 
+							   are hand-coded and developed for the unique needs of the
+							   user, using the best technologies and programming languages
 							   at our disposal.
 						   </p>
 						</div><!-- E_sub -->
 						<div class="sub last">
 							<h4>Technologies</h4>
-							<p>This site uses XHTML, CSS, jQuery &amp; PHP. All of the sites listed here 
-								were developed by Code-it Creations from code to CMS implementation. Our 
-								intention is to use clean, semantically correct code in all of our 
+							<p>This site uses XHTML, CSS, jQuery &amp; PHP. All of the sites listed here
+								were developed by Code-it Creations from code to CMS implementation. Our
+								intention is to use clean, semantically correct code in all of our
 								projects, insuring consistent rendering in all browser environments.
 							</p>
 						</div><!-- E_sub -->
@@ -240,7 +269,7 @@
 							<li><a href="#contact">Contact</a></li>
 						</ul>
 						<img src="images/smallLine.png" alt="" />
-					</div><!-- E_subnav -->	
+					</div><!-- E_subnav -->
 				</div><!-- E_section -->
 			</div><!-- E_content -->
 					<div id="copy">Copyright &copy; 2006 - <?php echo strftime('%Y', time()); ?> Code-it Creations - All Rights Reserved</div>
